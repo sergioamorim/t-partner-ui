36b15eecd6e56391f83f40182df793ab491cca6c
sergio <sata@ic.ufal.br>
2017-05-29 14:05:07 -0300

    modifications to receive a list of summaries

--- js/controllers/summary-controller.js
+++ js/controllers/summary-controller.js
@@ -1,10 +1,9 @@
-function summary_controller($scope, $filter, summaryAPI, studentAPI) {
+function summary_controller($scope, $filter, $timeout, summaryAPI, studentAPI) {
     $scope.requestData = {
         students: [],
         startDate: null,
         endDate: null
     };
-
     $scope.daterangepicker = {
         daterange: {
             startDate: null,
@@ -20,7 +19,8 @@ function summary_controller($scope, $filter, summaryAPI, studentAPI) {
                 'Last 7 Days': [moment().subtract(6, 'days'), moment()],
                 'Last 30 Days': [moment().subtract(29, 'days'), moment()],
                 'This Month': [moment().startOf('month'), moment().endOf('month')],
-                'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+                'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')],
+                'Last 2 Months': [moment().subtract(2, 'month').startOf('month'), moment().endOf('month')]
             },
             eventHandlers: {
                 'hide.daterangepicker': function() {
@@ -30,7 +30,7 @@ function summary_controller($scope, $filter, summaryAPI, studentAPI) {
             }
         }
     }
-    $scope.summaryData = [];
+    $scope.summaries = [];
     studentAPI.getStudents().then(function(data) {
         $scope.students = data.data;
     });
@@ -43,52 +43,41 @@ function summary_controller($scope, $filter, summaryAPI, studentAPI) {
             return itemText;
         }
     };
-    $scope.totalSummaryData = undefined;
-    $scope.summaryInterval = undefined;
+    $scope.completeSummaries = undefined;
+    $scope.summariesInterval = undefined;
     $scope.clearData = function() {
-        $scope.summaryData = [];
-        $scope.totalSummaryData = undefined;
-        $scope.summaryInterval = undefined;
+        $scope.summaries = [];
+        $scope.completeSummaries = undefined;
+        $scope.summariesInterval = undefined;
     }
-    $scope.requestSummary = function(requestData) {
-        requestDateFormat = "yyyy-MM-dd";
-        startDate = requestData.startDate;
-        endDateOriginal = requestData.endDate.clone().add(1, 'day');
-        if (endDateOriginal - startDate >= 1209600000) {
-            $scope.summaryInterval = "month";
-            while (startDate.clone().add(1, 'month') <= endDateOriginal) {
-                endDate = startDate.clone().add(1, 'month');
-                if (endDate > endDateOriginal) {
-                    endDate = endDateOriginal;
-                }
-                $scope.summaryData.push(summaryAPI.getSummaries(requestData));
-                startDate.add(1, 'month');
+    $scope.requestSummaries = function(requestData) {
+        summaryAPI.getSummaries(requestData).then(function(response) {
+            $scope.completeSummaries = response.data;
+        });
+        $timeout(function() {
+            console.log(requestData);
+            endDateOriginal = requestData.endDate;
+            if (endDateOriginal - requestData.startDate >= 1209600000) {
+                $scope.summariesInterval = 'month';
             }
-        }
-        else if (endDateOriginal - startDate >= 5011200000) {
-            $scope.summaryInterval = "week";
-            while (startDate.clone().add(1, 'week') <= endDateOriginal) {
-                endDate = startDate.clone().add(1, 'week');
-                if (endDate > endDateOriginal) {
-                    endDate = endDateOriginal;
-                }
-                $scope.summaryData.push(summaryAPI.getSummaries(requestData));
-                startDate.add(1, 'week');
+            else if (endDateOriginal - requestData.startDate >= 5011200000) {
+                $scope.summariesInterval = 'week';
             }
-        }
-        else if (endDateOriginal - startDate > 172800000) {
-            $scope.summaryInterval = "day";
-            while (startDate.clone().add(1, 'day') <= endDateOriginal) {
-                endDate = startDate.clone().add(1, 'day');
-                if (endDate > endDateOriginal) {
-                    endDate = endDateOriginal;
-                }
-                $scope.summaryData.push(summaryAPI.getSummaries(requestData));
-                startDate.add(1, 'day');
+            else if (endDateOriginal - requestData.startDate > 172800000) {
+                $scope.summariesInterval = 'day';
             }
-        }
-        $scope.totalSummaryData = summaryAPI.getSummaries(requestData);
-    };
+            while (requestData.startDate.clone().add(1, $scope.summariesInterval).isBefore(endDateOriginal)) {
+                requestData.endDate = requestData.startDate.clone().add(1, $scope.summariesInterval);
+                if (requestData.endDate > endDateOriginal) {
+                    requestData.endDate = endDateOriginal;
+                }
+                summaryAPI.getSummaries(requestData).then(function(response) {
+                    $scope.summaries.push(response.data);
+                });
+                requestData.startDate.add(1, $scope.summariesInterval);
+            }
+        }, 3000);
+    }
     $scope.showLearningGoals = function(learningGoals) {
         ModalService.showModal({
             templateUrl: '../../view/modals/learning-goals-modal.html',
--- js/services/summary-service.js
+++ js/services/summary-service.js
@@ -1,20 +1,7 @@
 angular.module("TP").factory("summaryAPI", function($http, config){
 	
 	var _getSummaries = function(requestData){
-        var req = {
-            method: 'POST',
-            url: config.baseUrl+'/student/summary',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            data: requestData
-        }
-        $http(req).then(function successCallback(response) {
-            return response.data;
-        }, function errorCallback(response) {
-            console.log(response);
-            return null;
-        });
+        return $http.post(config.baseUrl+'/student/summary', requestData);
 	}
 	
 	return {
